67c0eddf6af49f0d2a67ab3926e48c26401acfbf
Allison G. Endres <aendres19@gmail.com>
2017-03-29 07:21:03 -0400

    Add Program to evaluate even and oddnumbers inputed

--- EvenAndOdd/Program.cs
+++ EvenAndOdd/Program.cs
@@ -10,6 +10,34 @@ namespace EvenAndOdd
     {
         static void Main(string[] args)
         {
+            string input;
+
+            do
+            {
+                Console.WriteLine("Enter a number between 1 and 100: ");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number % 2 != 0)
+                {
+                    Console.WriteLine(number + " is Odd");
+                }
+                else if (number >= 2 && number % 2 == 0 && number <= 25)
+                {
+                    Console.WriteLine(number + " is Even and less than 25");
+                }
+
+                else if (number % 2 == 0 && number >= 26)
+                {
+                    Console.WriteLine(number + " is Even");
+                }
+
+                Console.WriteLine("Would you like to continue? (y/n)");
+                input = Console.ReadLine();
+            }
+            while (input.ToLower() == "y");
+            {
+                Console.WriteLine("Have a good day!");
+            }
         }
     }
 }
